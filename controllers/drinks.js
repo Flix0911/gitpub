d76e997d2cb9c0399bd5bc77b1161a2aa336d023
Erick Valencia <evalencia0911@gmail.com>
2023-12-17 12:25:33 -0700

    files talking and server running. Connect a go

---
+++ controllers/drinks.js
@@ -0,0 +1,10 @@
+//folder for routes
+
+//install express
+const express = require("express")
+
+//create a router
+const router = express.Router()
+
+//export the router
+module.exports = router
--- server.js
+++ server.js
@@ -7,22 +7,28 @@ const morgan = require("morgan")
 //import method override
 const methodOverride = require("method-override")
 
+//import drinks' router
+const drinksRouter = require("./controllers/drinks.js")
+
 //app object creation
 const app = express()
 
 //middleware
 
 //public folder for files
-app.arguments(express.static("public"))
+app.use(express.static("public"))
 
 //express.urlencoded (parse url encoded bodies) - add data to req.body
-app.arguments(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))
 
 //mogan variable - log data about request to check for potential errors
-app.arguments(morgan("dev"))
+app.use(morgan("dev"))
 
 //methodOverride
-app.arguments(methodOverride("_method"))
+app.use(methodOverride("_method"))
+
+//register drinksRouter
+app.use("/drinks", drinksRouter)
 
 //server listener to turn on server
 app.listen(3000, () => {
