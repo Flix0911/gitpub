c2b2d5b1ef3476d152e3c968a620915094734fc3
Erick Valencia <evalencia0911@gmail.com>
2023-12-17 14:00:20 -0700

    set up show route, id pulling

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -11,9 +11,16 @@ const drinks = require("../models/drinks.js")
 
 //routes
 
-//display drinks data in json
+//render drinks index page
 router.get("/", (req, res) => {
     res.render("index.ejs", {drinks})
+})
+
+//drinks route to id
+router.get("/:id", (req, res) => {
+    //id from params
+    //send the id as a response
+    res.send(req.params.id)
 })
 
 //export the router
